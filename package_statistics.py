5e3714f74d726f3c826ca988ee773ec17bc4e6a0
Alessandro Zanni <alessandro.zanni87@gmail.com>
2026-01-15 16:45:26 +0100

    pkgstats cli

---
+++ package_statistics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This is a helper file to use the pkgstats as requests from instructions
+
+Check the README.md for more information.
+"""
+
+from src.pkgstats.pkgstats import main
+
+if __name__ == "__main__":
+    main()
